56de6223ba3a109992a6549ffb889e7b1edb4f43
Kilian Agboton <agbotonstefane@gmail.com>
2024-06-26 14:37:14 -0400

    compared and styled generated and entered text
    - used fake-js to generate random sentence
    - handled user input and implemented conditional display
    - character by character comparison
    - used property binding for styling by comparison [classe]="compare(letter, ramdomText[i])"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'typing';
+
+  randomText: string = faker.lorem.sentence({ min: 3, max: 5 });
+  enteredText: string = '';
+  
+
+  onInput(event: Event) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) : string { 
+    if (!enteredLetter) return 'pending';
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect'
+  }
 }
